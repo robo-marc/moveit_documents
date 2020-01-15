02c85c7e4e81e4879728e70f8fdf2387404820a7
Yosuke Matsusaka <yosuke.matsusaka@gmail.com>
2020-01-15 10:45:16 +0900

    introduce breathe to import doxygen document from sphinx

--- source/conf.py
+++ source/conf.py
@@ -25,7 +25,10 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = []
+extensions = ['breathe']
+
+breathe_projects = { "moveit": "../doxygen/xml/" }
+breathe_default_project = "moveit"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
