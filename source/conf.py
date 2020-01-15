2f72b99220550261e0ca560c7c8eb01110a8d1db
Yosuke Matsusaka <yosuke.matsusaka@gmail.com>
2020-01-15 14:14:58 +0900

    introduce sphinxcontrib-ros to import message document from sphinx

--- source/conf.py
+++ source/conf.py
@@ -25,10 +25,12 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['breathe']
+extensions = ['breathe', 'sphinxcontrib.ros']
 
 breathe_projects = { "moveit": "../doxygen/xml/" }
 breathe_default_project = "moveit"
+
+ros_base_path = ['../doxygen']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
