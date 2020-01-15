dbd3657e3392fdb0418a3d6d722fa2e0775218a2
Yosuke Matsusaka <yosuke.matsusaka@gmail.com>
2020-01-15 14:18:35 +0900

    introduce sphinxcontrib-plantuml to write uml diagrams

--- source/conf.py
+++ source/conf.py
@@ -25,7 +25,7 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['breathe', 'sphinxcontrib.ros']
+extensions = ['sphinxcontrib.plantuml', 'breathe', 'sphinxcontrib.ros']
 
 breathe_projects = { "moveit": "../doxygen/xml/" }
 breathe_default_project = "moveit"
