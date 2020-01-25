e2d4827acf1f4c7bb24907e5925f2870e469b180
Yosuke Matsusaka <yosuke.matsusaka@gmail.com>
2020-01-25 09:20:48 +0900

    skip japanese on pretranslation

--- script/pretranslate.py
+++ script/pretranslate.py
@@ -1,6 +1,7 @@
 import sys
 import goslate
 import polib
+import langdetect
 import time
 from urllib.error import HTTPError
 
@@ -11,12 +12,17 @@ podata = polib.pofile(sys.argv[1])
 podata.metadata['PO-Revision-Date'] = time.strftime("%Y-%m-%d %H:%M%z"),
 
 for entry in podata:
-    if entry.msgstr == '':
+    langs = {}
+    for l in langdetect.detect_langs(entry.msgid):
+        langs[l.lang] = l.prob
+    print(entry.msgid, langs)
+    if entry.msgstr == '' and not langs.get('ja'):
         try:
             entry.msgstr = gs.translate(entry.msgid, 'ja')
-        except HTTPError:
-            pass
-        print(entry.msgid, entry.msgstr)
-        time.sleep(1)
+        except HTTPError as e:
+            print(e)
+            break
+        print(entry.msgstr)
+        time.sleep(3)
 
 podata.save(sys.argv[1])
