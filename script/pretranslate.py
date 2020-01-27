e418cdaff0aa12aff6485546677b3f557e28e19d
Yosuke Matsusaka <yosuke.matsusaka@gmail.com>
2020-01-27 12:26:37 +0900

    use official google translation api

--- script/pretranslate.py
+++ script/pretranslate.py
@@ -1,15 +1,17 @@
 import sys
-import goslate
 import polib
 import langdetect
+#import goslate
+from google.cloud import translate_v2 as translate
 import time
 from urllib.error import HTTPError
 
-gs = goslate.Goslate()
+#gs = goslate.Goslate()
+translate_client = translate.Client()
 
 podata = polib.pofile(sys.argv[1])
 
-podata.metadata['PO-Revision-Date'] = time.strftime("%Y-%m-%d %H:%M%z"),
+podata.metadata['PO-Revision-Date'] = time.strftime("%Y-%m-%d %H:%M%z")
 
 for entry in podata:
     langs = {}
@@ -18,11 +20,12 @@ for entry in podata:
     print(entry.msgid, langs)
     if entry.msgstr == '' and not langs.get('ja'):
         try:
-            entry.msgstr = gs.translate(entry.msgid, 'ja')
+            #entry.msgstr = gs.translate(entry.msgid, 'ja')
+            entry.msgstr = translate_client.translate(entry.msgid, target_language='ja')['translatedText']
         except HTTPError as e:
             print(e)
             break
         print(entry.msgstr)
-        time.sleep(3)
+        #time.sleep(3)
 
 podata.save(sys.argv[1])
